a312f1b3376b7be78ee78626e7163cdf32c67c96
macroing <joergen.lundgren@macroing.org>
2019-08-02 02:20:54 +0200

    Add copyright notice.

--- src/main/java/org/dayflower/pathtracer/main/Test.java
+++ src/main/java/org/dayflower/pathtracer/main/Test.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2015 - 2019 J&#246;rgen Lundgren
+ * 
+ * This file is part of Dayflower.
+ * 
+ * Dayflower is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Dayflower is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Dayflower. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.dayflower.pathtracer.main;
 
 import org.dayflower.pathtracer.util.Image;
